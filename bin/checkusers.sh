6b1b6abd3d6065c71575d24d9466c0d37e170b7f
Lampros Fasoulas <lamprosfasoulas@gmail.com>
2025-04-02 10:45:08 +0300

    checkusers.sh error fix

--- bin/checkusers.sh
+++ bin/checkusers.sh
@@ -3,7 +3,13 @@
 # It is vital to have RUN env > /etc/environment in Dockerfile so that
 # cronjob can have access to env variables from docker
 isActive() {
-  if ldapsearch -x -H $CMD_LDAP_URL -D "$CMD_LDAP_BINDDN" -w $CMD_LDAP_BINDCREDENTIALS -b $CMD_LDAP_SEARCHBASE $query | grep -q -e "^uid:"; then
+  if ldapres=$(ldapsearch -x -H $CMD_LDAP_URL -D "$CMD_LDAP_BINDDN" -w $CMD_LDAP_BINDCREDENTIALS -b $CMD_LDAP_SEARCHBASE $query 2>/dev/null); then
+    echo "Ldap is UP. Continue..."
+  else
+    echo "Ldap is DOWN. Exiting..."
+    exit 1
+  fi
+  if echo "$ldapres" | grep -q -e "^uid:"; then
     return 0
   else
     return 1
