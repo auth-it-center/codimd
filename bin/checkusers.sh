9e117c066bf01428d739549483dd26172977f54f
Lampros Fasoulas <lamprosfasoulas@gmail.com>
2025-04-09 13:01:10 +0300

    Modified checkusers.sh to move expired users' data to trash

--- bin/checkusers.sh
+++ bin/checkusers.sh
@@ -3,6 +3,8 @@
 # It is vital to have RUN env > /etc/environment in Dockerfile so that
 # cronjob can have access to env variables from docker
 . /home/hackmd/cron.env
+UPLOADS_DIR="/home/hackmd/app/public/uploads"
+TRASH_DIR="/home/hackmd/trash"
 isActive() {
   if ! ldapres=$(ldapsearch -x -H $CMD_LDAP_URL -D "$CMD_LDAP_BINDDN" -w $CMD_LDAP_BINDCREDENTIALS -b $CMD_LDAP_SEARCHBASE $query 2>/dev/null); then
     echo "Ldap is DOWN. Exiting..."
@@ -22,20 +24,48 @@ DELETE FROM "Users" WHERE profileid = '${profileid}';
 SQL
 }
 deleteNotes(){
-  # Deleting all user's notes from postgres database
-  local id="$1"
+  # Deleting all user's notes from postgres database and moving them to
+  # trash can
+shortids=$(psql $CMD_DB_URL -At<<-SQL
+SELECT shortid FROM "Notes" where "ownerId" = '${id}';
+SQL
+)
+while IFS=' ' read shortid; do
+title=$(psql $CMD_DB_URL -At<<-SQL
+SELECT content FROM "Notes" where shortid = '${shortid}';
+SQL
+)
+  safe_title=$(echo "$title" | tr -cd '[:alnum:]._-')
+  filename="${USER_TRASH_DIR}/${safe_title}.md"
+
+content=$(psql $CMD_DB_URL -At<<-SQL
+SELECT content FROM "Notes" WHERE shortid = '${shortid}';
+SQL
+)
+
+echo "$content" > "$filename"
+done<<<"$shortids"
+
 psql $CMD_DB_URL <<-SQL
 DELETE FROM "Notes" where "ownerId" = '${id}';
 SQL
 }
 deleteFiles(){
   # Deleting all user's uploads from location /home/hackmd/app/public/uploads
-  local id="$1"
   # The files are saved as upload_{user.id}_{random letters}.{jpg,png,...}
-  find /home/hackmd/app/public/uploads -type f -name "upload_${id}_*" -delete; 2>&1
+  find $UPLOADS_DIR -type f -name "upload_${id}_*" -exec mv {} "$USER_TRASH_DIR" \; 2>&1
+}
+actuallyDeleteFiles(){
+  # Deleting all user's uploads from location /home/hackmd/app/public/uploads
+  # The files are saved as upload_{user.id}_{random letters}.{jpg,png,...}
+
+  echo "Removing old trash files"
+  find $TRASH_DIR -mindepth 1 -maxdepth 1 -type d -mtime +90 -exec rm -rf {} \; 2>&1
 }
 # Write down the date-time
 date
+# Removing old trash files
+actuallyDeleteFiles
 allusers=$(psql $CMD_DB_URL -t -c 'SELECT profileid FROM "Users";')
 if [ -z "$allusers" ]; then
     echo "Ha Ha nothing to delete"
@@ -55,10 +85,15 @@ SQL
     echo "The account $uid  is active"
   else
     echo "The account $uid must be destroyed"
-    echo "Beggining destruction..."
+    # This will be the user specific trash can
+    USER_TRASH_DIR=${TRASH_DIR}/${uid}
+    echo "Moving to Trash ${USER_TRASH_DIR}..."
+    mkdir -p $USER_TRASH_DIR
+    # These functions move uploads and notes to trash can
+    # and delete them permanently after 90 days
     deleteUser $username
-    deleteNotes $id
-    deleteFiles $id
+    deleteNotes
+    deleteFiles
 
   fi
 done <<< "$allusers"
